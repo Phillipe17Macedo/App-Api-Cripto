069a86cfb8828dc54fd9296d8ca7f622cbfdab93
Phillipe <ph.stos.udia@gmail.com>
2024-04-14 22:10:50 -0300

    branch-dev-1b

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import getCryptoData from './api';
 
-export default function App() {
+const App = () => {
+  const [cryptoData, setCryptoData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCryptoData();
+      setCryptoData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Dados das Criptomoedas:</Text>
+        {cryptoData && cryptoData.map((crypto, index) => (
+          <View key={index} style={styles.cryptoContainer}>
+            <Image
+              source={{ uri: crypto.image }}
+              style={styles.cryptoLogo}
+            />
+            <View style={styles.cryptoInfo}>
+              <Text style={styles.cryptoName}>{crypto.name}</Text>
+              <Text style={styles.cryptoPrice}>Preço: ${crypto.price}</Text>
+              <Text style={styles.cryptoChange}>Variação (24h): {crypto.change}%</Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  scrollViewContent: {
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  cryptoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  cryptoLogo: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  cryptoInfo: {
+    flex: 1,
+  },
+  cryptoName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cryptoPrice: {
+    fontSize: 16,
+  },
+  cryptoChange: {
+    fontSize: 16,
+    color: 'green', // ou 'red' para negativo
+  },
 });
+
+export default App;
---
+++ api.js
@@ -0,0 +1,38 @@
+// api.js
+import axios from 'axios';
+
+const coinMarketCapUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+const coinMarketCapApiKey = 'e471b04e-c9eb-4d0a-9731-b0a0ea204af2';
+
+const getCryptoData = async () => {
+  try {
+    // Faz a chamada para a API do CoinMarketCap para obter os dados financeiros das criptomoedas
+    const responseMarketCap = await axios.get(coinMarketCapUrl, {
+      headers: {
+        'X-CMC_PRO_API_KEY': coinMarketCapApiKey,
+        'Accept': 'application/json',
+      }
+    });
+
+    console.log('Response from CoinMarketCap:', responseMarketCap.data);
+
+    // Retorna apenas os dados financeiros das criptomoedas
+    const cryptoData = responseMarketCap.data.data.map((crypto) => {
+      return {
+        name: crypto.name,
+        symbol: crypto.symbol,
+        price: crypto.quote.USD.price.toFixed(2),
+        change: crypto.quote.USD.percent_change_24h,
+      };
+    });
+
+    console.log('Crypto data:', cryptoData);
+
+    return cryptoData;
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+    return null;
+  }
+};
+
+export default getCryptoData;
