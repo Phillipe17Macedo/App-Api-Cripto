1160e2d5992c3e3dd442836ff4c25b487016f480
Phillipe <ph.stos.udia@gmail.com>
2024-04-14 22:15:03 -0300

    branch-dev-1c

--- App.js
+++ App.js
@@ -40,8 +40,9 @@ const App = () => {
 const styles = StyleSheet.create({
   scrollViewContent: {
     flexGrow: 1,
+    backgroundColor: '#3D3D3D',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
   },
   container: {
     flex: 1,
@@ -53,11 +54,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    color: '#84B026',
   },
   cryptoContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 15,
+    backgroundColor: '#217373',
+    padding: 10,
+    borderRadius: 10,
   },
   cryptoLogo: {
     width: 50,
@@ -70,13 +75,15 @@ const styles = StyleSheet.create({
   cryptoName: {
     fontSize: 18,
     fontWeight: 'bold',
+    color: '#fff',
   },
   cryptoPrice: {
     fontSize: 16,
+    color: '#fff',
   },
   cryptoChange: {
     fontSize: 16,
-    color: 'green', // ou 'red' para negativo
+    color: '#fff',
   },
 });
 
